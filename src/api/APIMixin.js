f1bb81b7472c8bd3ffe106083ea59f9f1e1f4ce9
Alexander Abaev <lowrydertrue@gmail.com>
2021-07-19 22:26:17 +1100

    Добавил Axios и axios-mock-adapter

--- src/api/APIMixin.js
+++ src/api/APIMixin.js
@@ -1,17 +1,22 @@
 // Подстановочные методы отправки данных
 
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const url = 'https://example.com';
+
+let mock = new MockAdapter(axios, { delayResponse: 2000 });
+mock.onPost(`${url}/saveData`).reply(200, {});
+mock.onPost(`${url}/changePassword`).reply(200, {});
+
 const api = {
   methods: {
-    apiSandData() {
-      return new Promise((resolve) => {
-        setTimeout(() => resolve(), 2000);
-      });
+    apiSandData(data) {
+      return axios.post(`${url}/saveData`, data);
     },
 
-    apiChangePassword() {
-      return new Promise((resolve) => {
-        setTimeout(() => resolve(), 2000);
-      });
+    apiChangePassword(data) {
+      return axios.post(`${url}/changePassword`, data);
     }
   }
 }
